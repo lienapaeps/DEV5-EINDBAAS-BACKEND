0d445f05e97df6ebe3304706413e6e9f6d3675ba
lienapaeps <paepsliena@gmail.com>
2022-11-29 20:08:07 +0100

    feat: add route users

---
+++ models/bestelling.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const bestellingSchema = new Schema({
+//     voornaam: { type: String, required: true },
+//     achternaam: { type: String, required: true },
+//     email: { type: String, required: true },
+//     telefoon: { type: String, required: true },
+//     straat: { type: String, required: true },
+//     huisnummer: { type: String, required: true },
+//     postcode: { type: String, required: true },
+//     gemeente: { type: String, required: true },
+//     opmerkingen: { type: String, required: false },
+//     donut: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'Donut'
+//     }
+// });
+
+// const Bestelling = mongoose.model('Bestelling', bestellingSchema);
+
+// module.exports = Bestelling;
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+// const usersController = require('../../../controllers/users');
+
+// login
+// router.post("/login", usersController.login);
+
+// register ???
+
+module.exports = router;
