9b771861bba79964e528696673bdbe57638231ba
lienapaeps <paepsliena@gmail.com>
2022-11-29 18:36:36 +0100

    feat: add api v1 to routes

--- routes/api/v1/donuts.js
+++ routes/api/v1/donuts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const donutsController = require('../controllers/donuts');
+const donutsController = require('../../../controllers/donuts');
 
 // get all donuts
 router.get('/', donutsController.getAll);
