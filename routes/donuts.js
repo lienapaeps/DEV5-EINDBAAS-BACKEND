a0c6314c9a8e71e3c6cd65a7ce33766a98a1ec4e
lienapaeps <paepsliena@gmail.com>
2022-11-29 18:34:56 +0100

    feat: add routes for /donuts

---
+++ routes/donuts.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const donutsController = require('../controllers/donuts');
+
+// get all donuts
+router.get('/', donutsController.getAll);
+
+// get donut by id
+router.get('/:id', donutsController.getById);
+
+// post/create donut
+router.post('/', donutsController.create);
+
+// update donut by id
+router.put('/:id', donutsController.update);
+
+// delete donut by id
+router.delete('/:id', donutsController.remove);
+
+module.exports = router;
