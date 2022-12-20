d4fbe51164c4bd0dd65c17caaea9fd21d96420f6
lienapaeps <paepsliena@gmail.com>
2022-12-20 23:18:41 +0100

    fix: size limit

--- app.js
+++ app.js
@@ -28,9 +28,12 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(express.limit('4M'));
+// app.use(express.bodyParser({limit: '2mb'}));
+// app.use(express.limit('4M'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb'}));
 
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
